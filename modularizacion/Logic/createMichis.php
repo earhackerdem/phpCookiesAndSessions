926905d14ae7293afd631bb1a3b1a7874e86a732
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2023-06-11 10:54:14 -0600

    Modularización del Código

---
+++ modularizacion/Logic/createMichis.php
@@ -0,0 +1,7 @@
+<?php
+
+require("Classes/Michi.php");
+
+$mrMichi = new Michi("Mr. Michi",'Blanquito',16);
+$michisancio = new Michi("Michisancio",'Naranja',14);
+$michales = new Michi("Michales",'Negro',15);
---
+++ modularizacion/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require ("Logic/createMichis.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li> <?= $mrMichi->getName() ?> dice: <?= $mrMichi->sayMeow() ?> </li>
+        <li> <?= $michisancio->getName() ?> dice: <?= $michisancio->sayMeow() ?> </li>
+        <li> <?= $michales->getName() ?> dice: <?= $michales->sayMeow() ?> </li>
+    </ul>
+</body>
+</html>
